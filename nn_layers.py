1d36916b49e6bd7910f27ed81e1b3387ca3fa3f9
Trodon <codyholdaway@hotmail.com>
2019-11-24 22:44:51 -0700

    A temp commit

--- nn_layers.py
+++ nn_layers.py
@@ -66,6 +66,12 @@ class DenseLayer:
         else:
             print("Unable to set weights as dimensions do not agree")
 
+    def set_bias(self, bias):
+        if bias.shape == (1, self.num_output_units):
+            self.bias = bias
+        else:
+            print("Unable to set bias as dimensions do not agree")
+
     def feed_forward(self, x):
         """
         Given a vector x in R^n, computes x.T * weights.T + bias and applies the activation function
@@ -75,3 +81,93 @@ class DenseLayer:
 
         """
         return self.activation_function(np.dot(x, self.weights.T) + self.bias)
+
+
+def crossover_layers(layer_1, layer_2):
+    """
+    Given two layers of the same type, perform crossover and produce a new layer of the same type as the parents.
+
+    Inputs:
+        layer_1 (nn layer) - The first layer used in crossover.
+        layer_2 (nn layer) - The second layer used in crossover.
+
+    Returns:
+        layer (nn layer) - A layer created from the crossover of the two input layers.
+
+    """
+    num_input_units = layer_1.num_input_units
+    num_output_units = layer_1.num_output_units
+    layer_type = layer_1.layer_type
+    weights = np.zeros(shape=(num_output_units, num_input_units))
+    bias = np.zeros(shape=(1, num_output_units))
+    if (num_input_units != layer_2.num_input_units) or (num_output_units != layer_2.num_output_units) or (
+            layer_type != layer_2.layer_type):
+        print("Unable to perform crossover as dimensions and/or type of layers do not agree")
+        return None
+    if np.random.choice([1, 2]) == 1:
+        activation_function = layer_1.activation_function
+    else:
+        activation_function = layer_2.activation_function
+    for i in range(num_output_units):
+        if np.random.choice([1, 2]) == 1:
+            weights[i, :] = layer_1.weights[i, :]
+            bias[0, i] = layer_1.weights[0, i]
+        else:
+            weights[i, :] = layer_2.weights[i, :]
+            bias[0, i] = layer_2.weights[0, i]
+    return DenseLayer(num_input_units, num_output_units, activation_function, weights, bias)
+
+
+def mutate_layer(layer, mutation_probability=0.01):
+    """
+    Given the weights and bias of a layer, we randomly and indepently mutate each number with probability
+    mutation_probability by replacing the weight/bias element by an observation from a uniform (-1, 1)
+    random variable
+
+    Inputs:
+        layer (nn layer) - A layer object whose weights and bias are to be mutated.
+        mutation_probability (float in (0, 1)) - The probability that a given element of the weights or bias will be
+                                                 mutated.
+
+    """
+    weights_to_mutate = np.random.choice(a=[0, 1], size=(layer.num_output_units, layer.num_input_units),
+                                         p=(1-mutation_probability, mutation_probability))
+    layer.set_weights(layer.weights * (1 - weights_to_mutate))
+    layer.set_weights(layer.weights + (2 * np.random.random(
+            size=(layer.num_output_units, layer.num_input_units)) - 1) * weights_to_mutate)
+
+    bias_to_mutate = np.random.choice(a=[0, 1], size=(1, layer.num_output_units),
+                                      p=(1-mutation_probability, mutation_probability))
+    layer.set_bias(layer.bias * (1 - bias_to_mutate))
+    layer.set_bias(layer.bias + (2 * np.random.random(size=(1, layer.num_output_units)) - 1) * bias_to_mutate)
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
